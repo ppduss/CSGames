865b4337a452a72431ef9514ac41f46e54230e52
Patrick <ppduss@gmail.com>
2021-01-11 14:00:05 -0500

    countdown section css complete

---
+++ index.js
@@ -0,0 +1,8 @@
+import dayjs from src = "node_modules/dayjs/dayjs.min.js";
+
+    // import relativeTime from 'dayjs/plugin/advancedFormat'
+    // dayjs.extend(relativeTime); 
+  
+  let now = dayjs();
+  document.getElementById("days").innerHTML = "hey";
+  console.log(now)
