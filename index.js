dc6c592e88c87bbaed93735b3f6cdb1b4a8a8a98
Patrick <ppduss@gmail.com>
2021-01-12 13:44:44 -0500

    Done!

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 
 // --- I set the date to 2021 so it actually runs! --- //
-var eventDate = new Date("Oct 30, 2021 12:00:00").getTime();
+const eventDate = new Date("Oct 30, 2021 12:00:00").getTime();
 
-var counter = setInterval(function() {
-  var today = new Date().getTime();
-  var timeGap = eventDate - today;
+const counter = setInterval(function() {
+  const today = new Date().getTime();
+  const timeGap = eventDate - today;
 
-  var days = Math.floor(timeGap / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((timeGap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((timeGap % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((timeGap % (1000 * 60)) / 1000);
+  const days = Math.floor(timeGap / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeGap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeGap % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeGap % (1000 * 60)) / 1000);
 
   document.getElementById("days").innerHTML = days;
   document.getElementById("hours").innerHTML = hours;
   document.getElementById("minutes").innerHTML = minutes;
   document.getElementById("seconds").innerHTML = seconds;
   document.getElementById("consolidatedTime").innerHTML = days+"D "+hours+"H "+minutes+"M "+seconds+"S ";
-
 
   if (minutes < 0) {
     clearInterval(counter);
