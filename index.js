7153c6f677d07ada71b725d4704cbf02bb96cf47
Patrick <ppduss@gmail.com>
2021-01-12 11:05:53 -0500

    counter finished

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-// import { relativeTime } from 'dayjs/locale/*';
-// import dayjs from src = "node_modules/dayjs/dayjs.min.js";
-import moment from 'moment';
 
-const moment = require('moment');
-const date = moment();
+// --- I set the date to 2021 so it actually runs! --- //
+var eventDate = new Date("Oct 30, 2021 12:00:00").getTime();
 
-  
-document.getElementById("days").innerHTML = "hey";
-console.log(date)
-  
+var counter = setInterval(function() {
+  var today = new Date().getTime();
+  var timeGap = eventDate - today;
 
-  // let now = dayjs();
-  // document.getElementById("days").innerHTML = "hey";
-  // console.log(now)
+  var days = Math.floor(timeGap / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((timeGap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((timeGap % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((timeGap % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  if (minutes < 0) {
+    clearInterval(counter);
+    document.getElementById("days").innerHTML = "NOW";
+    document.getElementById("hours").innerHTML = "NOW";
+    document.getElementById("minutes").innerHTML = "NOW";
+    document.getElementById("seconds").innerHTML = "NOW";
+  }
+}, 1000);
